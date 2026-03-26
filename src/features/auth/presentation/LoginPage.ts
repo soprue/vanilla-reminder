d1c9086362b43c7ce606252bf74ef25d47ebc338
Gahee Kim <62260343+soprue@users.noreply.github.com>
2026-03-26 15:57:26 +0900

    feat: AuthStore 구현 및 로그인 페이지 전역 상태 적용

--- src/features/auth/presentation/LoginPage.ts
+++ src/features/auth/presentation/LoginPage.ts
@@ -1,15 +1,37 @@
 import { Component, ComponentProps } from '@core/Component';
 import jsx, { createDOM } from '@core/JSX';
+import { authStore } from '@src/shared/store/AuthStore';
 
 export default class LoginPage extends Component<ComponentProps> {
-  constructor(target: HTMLElement, props: ComponentProps) {
-    super(target, props);
+  init() {
+    this.subscribe(authStore);
+  }
+
+  handleLogin() {
+    authStore.login('홍길동');
+  }
+
+  handleLogout() {
+    authStore.logout();
   }
 
   render() {
+    const { isLoggedIn, user } = authStore.getState();
+
     return jsx`
           <div>
             <h1>Login Page</h1>
+            ${
+              isLoggedIn
+                ? jsx`<div><p>${
+                    user?.name
+                  }님 환영합니다.</p><button onclick="${this.handleLogout.bind(
+                    this
+                  )}">로그아웃</button></div>`
+                : jsx`<div><p>로그인이 필요합니다.</p><button onclick="${this.handleLogin.bind(
+                    this
+                  )}">로그인</button></div>`
+            }
           </div>
     `;
   }
